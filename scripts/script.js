0419bf4435036a09798e789ae80f2b07b8b31a3e
Jermo Barberereaux Swaab <jermo@Jermos-MacBook-Air.local>
2021-09-19 21:59:05 +0200

    index.html: added new containers, ids and classes so data could be inserted with javascript. script.js: added a fetch for show information, show data via innerHTML and added onclick function. style.css: applied all styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,89 @@
+// Fetch data from tvmaze API
 function fetchData() {
-   fetch('https://api.tvmaze.com/shows/1955/episodes').then (response => {
-       return response.json()
-   }).then(data => {
-    console.log(data);
-   })
+    // Fetch episode list & details
+    fetch('https://api.tvmaze.com/shows/1955/episodes')
+        .then (response => {
+            //check if response is true, if not throw error.
+            if (!response.ok) {
+                throw Error("ERROR, Failed to fetch API");
+            }
+            return response.json()
+        })
+        .then(data => {
+            //Loop through array to obtain values.
+            const html = data.map(episode => {
+                //prepare values and HTML
+               return `
+               <div class='container'>
+                   <h3>${episode.name}</h3>
+                   <div class='details'>
+                        <img src='${episode.image.medium}'/>
+                        ${episode.summary}
+                   </div>
+               </div>
+            `;
+            // Get rid of kommas with .join('').
+            }).join('')
+
+            // inject HTML into respective element.
+            document.querySelector('#app').innerHTML = html;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    
+    //fetch show information
+    fetch('https://api.tvmaze.com/shows/1955')
+        .then(response => {
+            //check if response is true, if not throw error.
+            if (!response.ok) {
+                throw Error("ERROR, Failed to fetch API");
+            }
+            return response.json()
+            .then(data => {
+                //obtain data
+                const summary = data.summary;
+                const showtitle = data.name;
+                const image = data.image.original;
+                
+                //inject data into html elements
+                document.querySelector('#summary').innerHTML = summary;
+                document.querySelector('#showtitle').innerHTML = showtitle;
+                //cover image gets added to the hero background.
+                document.querySelector('#hero').style.backgroundImage = `url('${image}')`;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        })
 }
 
+// call function
 fetchData();
+
+//Hero script
+var hero = function() {
+    var hero = $('.hero-nav'),
+        heroHeight = $('.hero-nav').outerHeight(true);
+        $(hero).parent().css('padding-top', heroHeight);
+    $(window).scroll(function() {
+        var scrollOffset = $(window).scrollTop();
+        if (scrollOffset < heroHeight) {
+            $(hero).css('height', (heroHeight - scrollOffset));
+        }
+        if (scrollOffset > (heroHeight - 215)) {
+            hero.addClass('fixme');
+        } else {
+            hero.removeClass('fixme');
+        };
+    });
+}
+hero();
+
+
+//on click toggle details of episodes.
+$(document).ready(function() {
+    $(document).on('click', '.container' ,function (e) {
+        $(this).find('.details').slideToggle();
+    });
+});
